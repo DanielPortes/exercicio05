b8004f778aa77b4f83d97e760bfd7803aabf77f3
Daniel <fagundes.daniel191@gmail.com>
2025-02-13 22:37:51 -0300

    Isto eh um commit automatico exercicio05

---
+++ src/main/java/padroescriacao/exercicio05/OleriteVisitorXml.java
@@ -0,0 +1,24 @@
+package padroescriacao.exercicio05;
+
+public class OleriteVisitorXml implements Visitor {
+    @Override
+    public String visitarSupervisor(Supervisor supervisor) {
+        return ("Supervisor: " + supervisor.getNome()
+                + "Possui o salário de R$ 500,00"
+                + " <br>");
+    }
+
+    @Override
+    public String visitarDiretor(Diretor diretor) {
+        return ("Diretor: " + diretor.getNome()
+                + "Possui o salário de R$ 150.000,00"
+                + " <br>");
+    }
+
+    @Override
+    public String visitarPeao(Peao peao) {
+        return "Peão: " + peao.getNome()
+                + "Possui o salário de 1 un de arroz"
+                + " <br>";
+    }
+}
--- src/test/java/padroescriacao/exercicio05/OleriteVisitorTest.java
+++ src/test/java/padroescriacao/exercicio05/OleriteVisitorTest.java
@@ -7,38 +7,117 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class OleriteVisitorTest {
-    @BeforeEach
-    public void setUp() {
+    @Test
+    public void testVisitarDiretorCsv()
+    {
         Diretor diretor = new Diretor("Diretor");
+        OleriteVisitorJson oleriteVisitor = new OleriteVisitorJson();
+        assertEquals("Diretor: DiretorPossui o salário de R$ 150.000,00 Json", oleriteVisitor.visitarDiretor(diretor));
+    }
+
+    @Test
+    public void testVisitarSupervisorCsv()
+    {
         Supervisor supervisor = new Supervisor("Supervisor");
+        OleriteVisitorJson oleriteVisitor = new OleriteVisitorJson();
+        assertEquals("Supervisor: SupervisorPossui o salário de R$ 500,00 Json", oleriteVisitor.visitarSupervisor(supervisor));
+    }
+
+    @Test
+    public void testVisitarPeaoCsv()
+    {
         Peao peao = new Peao("Peão");
+        OleriteVisitorJson oleriteVisitor = new OleriteVisitorJson();
+        assertEquals("Peão: PeãoPossui o salário de 1 un de arroz Json", oleriteVisitor.visitarPeao(peao));
     }
 
-    @Test
-    public void testOleriteVisitor() {
-        OleriteVisitor oleriteVisitor = new OleriteVisitor();
-        assertNotNull(oleriteVisitor);
-    }
 
     @Test
-    public void testVisitarDiretor() {
+    public void testVisitarDiretorJson()
+    {
         Diretor diretor = new Diretor("Diretor");
-        OleriteVisitor oleriteVisitor = new OleriteVisitor();
-        assertEquals("Diretor: DiretorPossui o salário de R$ 150.000,00", oleriteVisitor.visitarDiretor(diretor));
+        OleriteVisitorJson oleriteVisitor = new OleriteVisitorJson();
+        assertEquals("Diretor: DiretorPossui o salário de R$ 150.000,00 Json", oleriteVisitor.visitarDiretor(diretor));
     }
 
     @Test
-    public void testVisitarSupervisor() {
+    public void testVisitarSupervisorJson()
+    {
         Supervisor supervisor = new Supervisor("Supervisor");
-        OleriteVisitor oleriteVisitor = new OleriteVisitor();
-        assertEquals("Supervisor: SupervisorPossui o salário de R$ 500,00", oleriteVisitor.visitarSupervisor(supervisor));
+        OleriteVisitorJson oleriteVisitor = new OleriteVisitorJson();
+        assertEquals("Supervisor: SupervisorPossui o salário de R$ 500,00 Json", oleriteVisitor.visitarSupervisor(supervisor));
     }
 
     @Test
-    public void testVisitarPeao() {
+    public void testVisitarPeaoJson() {
         Peao peao = new Peao("Peão");
-        OleriteVisitor oleriteVisitor = new OleriteVisitor();
-        assertEquals("Peão: PeãoPossui o salário de 1 un de arroz", oleriteVisitor.visitarPeao(peao));
+        OleriteVisitorJson oleriteVisitor = new OleriteVisitorJson();
+        assertEquals("Peão: PeãoPossui o salário de 1 un de arroz Json", oleriteVisitor.visitarPeao(peao));
+    }
+
+    @Test
+    public void testVisitarSupervisorXml()
+    {
+        Supervisor supervisor = new Supervisor("Supervisor");
+        OleriteVisitorJson oleriteVisitor = new OleriteVisitorJson();
+        assertEquals("Supervisor: SupervisorPossui o salário de R$ 500,00 Json", oleriteVisitor.visitarSupervisor(supervisor));
+    }
+
+    @Test
+    public void testVisitarDiretorXml()
+    {
+        Diretor diretor = new Diretor("Diretor");
+        OleriteVisitorJson oleriteVisitor = new OleriteVisitorJson();
+        assertEquals("Diretor: DiretorPossui o salário de R$ 150.000,00 Json", oleriteVisitor.visitarDiretor(diretor));
+    }
+
+    @Test
+    public void testVisitarPeaoXml() {
+        Peao peao = new Peao("Peão");
+        OleriteVisitorJson oleriteVisitor = new OleriteVisitorJson();
+        assertEquals("Peão: PeãoPossui o salário de 1 un de arroz Json", oleriteVisitor.visitarPeao(peao));
+
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    @Test
+//    public void testOleriteVisitor() {
+//        OleriteVisitorJson oleriteVisitor = new OleriteVisitorJson();
+//        assertNotNull(oleriteVisitor);
+//    }
+//
+//    @Test
+//    public void testVisitarDiretor() {
+//        Diretor diretor = new Diretor("Diretor");
+//        OleriteVisitorJson oleriteVisitor = new OleriteVisitorJson();
+//        assertEquals("Diretor: DiretorPossui o salário de R$ 150.000,00", oleriteVisitor.visitarDiretor(diretor));
+//    }
+//
+//    @Test
+//    public void testVisitarSupervisor() {
+//        Supervisor supervisor = new Supervisor("Supervisor");
+//        OleriteVisitorJson oleriteVisitor = new OleriteVisitorJson();
+//        assertEquals("Supervisor: SupervisorPossui o salário de R$ 500,00", oleriteVisitor.visitarSupervisor(supervisor));
+//    }
+//
+//    @Test
+//    public void testVisitarPeao() {
+//        Peao peao = new Peao("Peão");
+//        OleriteVisitorJson oleriteVisitor = new OleriteVisitorJson();
+//        assertEquals("Peão: PeãoPossui o salário de 1 un de arroz", oleriteVisitor.visitarPeao(peao));
+//    }
+//
+//}
